eccfb3aec768426afc0fbb79f9f835c5a9983d0c
Jonas Andrade <devzut@gmail.com>
2025-10-01 08:48:10 -0300

    update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,29 @@ import {
   // Outlet,
   RouterProvider,
 } from 'react-router-dom';
-import { AuthProvider, useAuth } from '@/auth/auth-context';
+import { AuthProvider } from '@/auth/auth-context'; //useAuth
 import ProtectLayout from '@/components/layouts/protect-layout';
 import PublicLayout from '@/components/layouts/public-layout';
 import './App.css'
 
 import { ThemeProvider } from "@/components/theme/theme-provider"
-import { ModeToggle } from "@/components/theme/mode-toggle"
 
-import { SignIn } from "@/components/pages/sign/sign-in"
-
+import { SignUp } from "@/components/pages/sign/sign-up";
+import { SignIn } from "@/components/pages/sign/sign-in";
+import { Forgot } from './components/pages/sign/forgot';
 
 
 // Example Pages
 const HomePage = () => <h2>Home Page</h2>;
-const LoginPage = () => {
-  // const { login } = useAuth();
-  return (
-    <div>
-      <h2>Login Page</h2>
-      {/* <button onClick={login}>Log In</button> */}
-    </div>
-  );
-};
+// const LoginPage = () => {
+//   // const { login } = useAuth();
+//   return (
+//     <div>
+//       <h2>Login Page</h2>
+//       {/* <button onClick={login}>Log In</button> */}
+//     </div>
+//   );
+// };
 
 const DashboardPage = () => <h2>Dashboard Page</h2>;
 
@@ -35,7 +35,9 @@ const router = createBrowserRouter([
     element: <PublicLayout />,
     children: [
       { path: '/', element: <HomePage /> },
+      { path: '/create', element: <SignUp /> },
       { path: '/login', element: <SignIn /> },
+      { path: '/forgot', element: <Forgot /> },
     ],
   },
   {
